1585ef8f4734f8b5cc6b9f186e223f848a1cc27c
WhisperedShadow <dd9514371@gmail.com>
2025-03-25 17:12:39 +0530

    SOme changes

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -33,23 +33,35 @@ const Footer = () => {
   const Contents = [quickLinks, socialMedia];
 
   return (
-    <section className={styles.footerSection}>
-      {Contents.map((content) => (
-        <div key={content.id} className={styles.footerContent}>
-          <h3>{content.name}</h3>
-          <ul>
-            {content.links.map((link) => (
-              <li key={link.text} className={styles.footerLink}>
-                <a href={link.url} target="_blank" rel="noopener noreferrer">
-                  <i className={link.icon}></i>
-                  <p> {link.text}</p>
-                </a>
-              </li>
-            ))}
-          </ul>
-        </div>
-      ))}
-    </section>
+    <>
+      <section className={styles.footerSection}>
+        {Contents.map((content) => (
+          <div key={content.id} className={styles.footerContent}>
+            <h3>{content.name}</h3>
+            <ul>
+              {content.links.map((link) => (
+                <li key={link.text} className={styles.footerLink}>
+                  <a href={link.url} target="_blank" rel="noopener noreferrer">
+                    <i className={link.icon} style={{ marginRight: "8px" }}></i>
+                    <p>{link.text}</p>
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))}
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2174.313161337269!2d80.218972389064!3d12.866468900234176!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a525b1b94bbc64b%3A0xc8589bd91ae8d1cf!2sCSE%20department%20of%20Jeppiaar%20Engineering%20college!5e0!3m2!1sen!2sin!4v1742901564600!5m2!1sen!2sin"
+          width="600"
+          height="450"
+          style={{ border: 0 }}
+          allowFullScreen
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+          className={styles.map}
+        ></iframe>
+      </section>
+    </>
   );
 };
 
--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React from "react";
 
 const Contact = () => {
-  return (
-    <div>Contact</div>
-  )
-}
+  const studentCoordinates = {
+    name: "Student Coordinates",
+    memebers: [
+      { name: "Harish E", number: "6381524202" },
+      { name: "Anusiya T", number: "6383304738" },
+      { name: "Priyadharshini R", number: "9025611732" },
+      { name: "Harish", number: "8610937144" },
+    ],
+  };
 
-export default Contact
+  const eventOrganizers = {
+    name: "Event Organizers",
+    memebers: [
+      { name: "Abraham Kinston A", number: "8903550492" },
+      { name: "Ashwin K", number: "9360349760" },
+    ],
+  };
+
+  const organizations = [studentCoordinates, eventOrganizers];
+
+  return( <section>
+    <h1>Contact Page
+{organizations.map((organization)=>{
+  return (
+    <div key={organization.name}>
+      <h2>{organization.name}</h2>
+      {organization.memebers.map((membar)=>{
+        return (
+          <div key={membar.name}>
+            <p>{membar.name}</p>
+            <p>{membar.number}</p>
+          </div>
+        )
+      })}
+    </div>
+  )
+})}
+
+    </h1>
+  </section>);
+};
+
+export default Contact;
