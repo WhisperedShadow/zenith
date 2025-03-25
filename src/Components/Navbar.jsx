2a12ea7e0aea189b38b1a83b30f092f430e54d23
abinesh0309 <abinesh0309@gmail.com>
2025-03-25 16:01:47 +0530

    homepage added

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,32 +1,35 @@
-import React from "react";
-import styles from "./Navbar.module.css";
+// Navbar.jsx
+import React, { useState } from 'react';
+import styles from './Navbar.module.css';
+
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div>
       <nav className={styles.navbar}>
-        <img
-          src="/logo/nklogo.png"
-          alt=""
-          srcset=""
-          className={styles.logoimg}
-        />
-        <ul className={styles.navbarList}>
-          <li>
-            <a href="/">HOME</a>
-          </li>
-          <li>
-            <a href="/tech">TECHNICAL</a>
-          </li>
-          <li>
-            <a href="/nonetech">NON-TECHNICAL</a>
-          </li>
-          <li>
-            <a href="/contact">CONTACT</a>
-          </li>
+        <img src="/logo/nklogo.png" alt="" className={styles.logoimg}/>
+        
+        {/* Hamburger Icon */}
+        <div 
+          className={styles.hamburger} 
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          <div className={styles.line}></div>
+          <div className={styles.line}></div>
+          <div className={styles.line}></div>
+        </div>
+
+        {/* Navigation Links */}
+        <ul className={`${styles.navbarList} ${isOpen ? styles.active : ''}`}>
+          <li><a href="/">HOME</a></li>
+          <li><a href="/tech">TECHNICAL</a></li>
+          <li><a href="/nonetech">NON-TECHNICAL</a></li> 
+          <li><a href="/contact">CONTACT</a></li>
         </ul>
       </nav>
     </div>
   );
-};
+}
 
 export default Navbar;
